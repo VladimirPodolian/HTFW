4ee33678a39d9342a0e37a72136b1d729dade526
PVladimir <vladimir.podolyan@techops.ru>
2022-02-16 04:19:15 +0300

    small updates

--- framework/utils.py
+++ framework/utils.py
@@ -9,3 +9,14 @@ logging.basicConfig(
 
 def log(msg):
     return logging.info(msg)
+
+
+def cut_log_data(data: str, length=50) -> str:
+    """
+    Cut given data for reducing log length
+
+    :param data: original data ~ 'very long string for typing. string endless continues'
+    :param length: length to cut given data ~ 20
+    :return: edited data ~ 'Type text: "very long string for >>> 36 characters"'
+    """
+    return f'{data[:length]} >>> {len(data[length:])} characters' if len(data) > length else data
